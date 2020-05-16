495b8108a6107b3edef0141b7bcc66a41c6734d5
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-16 22:23:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.select do |x|
-    x=("Earth!","Wind!","Fire!","Water!", "Heart!")
+    ("Earth!","Wind!","Fire!","Water!", "Heart!")
     
   
 end
